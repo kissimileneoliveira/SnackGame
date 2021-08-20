13ad17a1c34a5c2d04ff7af410a09939e461c841
kissimilene <milenekisslove@hotmail.com>
2021-08-20 14:09:24 -0300

    defitnir relacao e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d");
 let box = 32;
-let snake = [];
+let snake = [];  //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
 let direction = "right";
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -20,9 +24,38 @@ function criarCobrinha(){
     }
 }
 
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+//quando um evento acontece, detecta e chama uma função
+document.addEventListener('keydown' , update);
+
+function update (event){
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right"; 
+    if (event.keyCode == 40 && direction != "up") direction = "down";    
+}
+
 function iniciarJogo(){
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y 16 * box;
+
+    for(i = i; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Gamer Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
